5699f204831f82771c44a1deb30d432d5dc02695
Abigail Rolling and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-25 11:54:59 -0700

    make initial list of model attributes

---
+++ app/src/main/java/com/epicodus/weatherapp/models/Forecast.java
@@ -0,0 +1,28 @@
+package com.epicodus.weatherapp.models;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class Forecast {
+    private String mCityName;
+    private int mCityId;
+    private String mCountry;
+    private int mDate; // FIX ME TO JAVA DATE
+    private double mDayTemperature;
+    private double mMaxDayTemperature;
+    private double mMinDayTemperature;
+    private double mNightTemperature;
+    private double mEveningTemperature;
+    private double mMorningTemperature;
+    private double mPressure;
+    private double mHumidity;
+    private int mWeatherId;
+    private String mWeatherMain;
+    private String mDescription;
+    private String mIcon;
+    private double mWindSpeed;
+    private int mWindDirection;
+    private int mCloudPercentage;
+    private double mRainPercentage;
+    private double mSnowPercentage;
+}
